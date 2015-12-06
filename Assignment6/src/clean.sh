0060e520a8f88161dc4ff3e814345a85f29a17bf
Bradford Smith <bsmith8@stevens.edu>
2015-12-06 00:55:44 -0500

    Added a cleanup script and a submit script
    In `src/` `./clean.sh` and `./submit.sh`

---
+++ Assignment6/src/clean.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+################################################################################
+# Bradford Smith (bsmith8)
+# CS 511 Assignment 6 clean.sh
+# 12/06/2015
+# "I pledge my honor that I have abided by the Stevens Honor System."
+################################################################################
+
+rm -f *.aut
+rm -f *.trl
+
---
+++ Assignment6/src/submit.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+################################################################################
+# Bradford Smith (bsmith8)
+# CS 511 Assignment 6 submit.sh
+# 12/06/2015
+# "I pledge my honor that I have abided by the Stevens Honor System."
+################################################################################
+
+./clean.sh
+tar -cvzf ../../bsmith8_cs511-assignment6.tar.gz ../*
+
